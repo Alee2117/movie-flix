5d54fdd7898b23c1610e04173cb8c72f1fc07c09
Austin Lee <alee2117@gmail.com>
2020-02-17 17:10:10 -0500

    Call to api works and logs in console

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,17 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Axios from "axios";
 
 const Search = () => {
   const [movies, setMovies] = useState("");
+  const [search, setSearch] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const handleInput = e => {
-    console.log(e.target.value);
+    setMovies(e.target.value);
+    setSearch(e.target.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(e.target.value);
+    setIsLoaded(true);
+    // setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    if (isLoaded) {
+      getMovies(search);
+      setIsLoaded(false);
+    }
+  });
+
+  const getMovies = search => {
+    Axios.get(`http://www.omdbapi.com/?t=${search}&apikey=183ac7ca`).then(
+      res => {
+        console.log(res.data);
+      }
+    );
   };
 
   return (
@@ -23,6 +43,7 @@ const Search = () => {
         <span className="search">Search</span>
         <input onChange={handleInput} className="search-input" type="text" />
       </form>
+      <p>{movies}</p>
     </div>
   );
 };
