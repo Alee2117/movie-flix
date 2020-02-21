28f7bee68a0e054a1e2cb76f8be5a61f1a1b9bdf
Austin Lee <alee2117@gmail.com>
2020-02-21 15:32:02 -0500

    styling is a mess but I'm getting it started. It's designed in Figma, it just needs to be built

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -11,18 +11,23 @@ const Movie = ({ movies }) => {
         alt=""
         src={movie.poster}
       ></img>
-      <div className="movie-info">
+      <div className="title-runtime">
         <h1 className="title" key={movie.title}>
           {movie.title}
         </h1>
         <small>{movie.runtime}</small>
-        <h3>{movie.plot}</h3>
+      </div>
+      <div className="rating-genre">
         <h1 key={movie.id}>{movie.rated}</h1>
         <h1>{movie.genre}</h1>
+      </div>
+      <div className="movie-info">
+        <h3 className="plot">{movie.plot}</h3>
         <small>Released: {movie.released}</small>
         <p>Director: {movie.director}</p>
         <p>Writers: {movie.writer}</p>
         <p>Leading Actors: {movie.actors}</p>
+        <p>Awards won: {movie.awards}</p>
       </div>
     </div>
   ));
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -28,27 +28,27 @@ const Search = () => {
   }, [isLoaded]);
 
   const getMovies = search => {
-    Axios.get(`http://www.omdbapi.com/?t=${search}&apikey=183ac7ca`).then(
-      res => {
-        console.log(res.data);
-        let movieObject = {
-          title: res.data.Title,
-          poster: res.data.Poster,
-          rated: res.data.Rated,
-          released: res.data.Released,
-          runtime: res.data.Runtime,
-          genre: res.data.Genre,
-          director: res.data.Director,
-          writer: res.data.Writer,
-          actors: res.data.Actors,
-          plot: res.data.Plot,
-          awards: res.data.Awards,
-          id: res.data.imdbID
-        };
+    Axios.get(
+      `http://www.omdbapi.com/?t=${search}&plot=full&apikey=183ac7ca`
+    ).then(res => {
+      console.log(res.data);
+      let movieObject = {
+        title: res.data.Title,
+        poster: res.data.Poster,
+        rated: res.data.Rated,
+        released: res.data.Released,
+        runtime: res.data.Runtime,
+        genre: res.data.Genre,
+        director: res.data.Director,
+        writer: res.data.Writer,
+        actors: res.data.Actors,
+        plot: res.data.Plot,
+        awards: res.data.Awards,
+        id: res.data.imdbID
+      };
 
-        setMovie([...movies, movieObject]);
-      }
-    );
+      setMovie([...movies, movieObject]);
+    });
   };
 
   return (
