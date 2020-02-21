fd6f379f1fcb0d877ff40ae0bfcf830e6964bf1f
Austin Lee <alee2117@gmail.com>
2020-02-21 18:20:39 -0500

    Styled it how I want it. Need to fix reload after first search

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -4,30 +4,38 @@ import "./Movie.css";
 const Movie = ({ movies }) => {
   return movies.map(movie => (
     <div key={movie.id}>
-      <img
-        height="450"
-        width="350"
-        className="poster"
-        alt=""
-        src={movie.poster}
-      ></img>
-      <div className="title-runtime">
-        <h1 className="title" key={movie.title}>
-          {movie.title}
-        </h1>
-        <small>{movie.runtime}</small>
+      <div className="poster-title">
+        <ul>
+          <li>
+            <img
+              height="450"
+              width="350"
+              className="poster"
+              alt=""
+              src={movie.poster}
+            ></img>
+          </li>
+          <li>
+            <h1 className="title" key={movie.title}>
+              {movie.title}
+            </h1>
+            <small>{movie.runtime}</small>
+          </li>
+        </ul>
       </div>
       <div className="rating-genre">
-        <h1 key={movie.id}>{movie.rated}</h1>
+        <h1 className="rating" key={movie.id}>
+          {movie.rated}
+        </h1>
         <h1>{movie.genre}</h1>
       </div>
+      <h3 className="plot">{movie.plot}</h3>
       <div className="movie-info">
-        <h3 className="plot">{movie.plot}</h3>
-        <small>Released: {movie.released}</small>
         <p>Director: {movie.director}</p>
-        <p>Writers: {movie.writer}</p>
+        <p>Writer(s): {movie.writer}</p>
         <p>Leading Actors: {movie.actors}</p>
-        <p>Awards won: {movie.awards}</p>
+        <p>{movie.awards}</p>
+        <small className="released">Released: {movie.released}</small>
       </div>
     </div>
   ));
